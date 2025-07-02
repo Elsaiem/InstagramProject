6f0609ec9c0069e5836dd8ba25c731a6d9828c20
Ziad <ziad@example.com>
2025-07-02 21:44:55 +0300

    add search request and response

---
+++ InstagramProject.Core/Contracts/Home/SearchResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramProject.Core.Contracts.Home
+{
+	public record SearchResponse
+	(
+		string Id,
+		string Name,
+		string Image
+	);
+}
