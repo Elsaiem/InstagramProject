afb09b556f056a79fb3fb5c4336824b6b8d66e6f
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-07-09 01:39:14 +0300

    add self relation for comment table

--- InstagramProject.Core/Entities/Comment.cs
+++ InstagramProject.Core/Entities/Comment.cs
@@ -12,10 +12,13 @@ namespace InstagramProject.Core.Entities
 		public int Id { get; set; }
 		public string Content { get; set; } = default!;
 		public int PostId { get; set; }
+		public int? ParentCommentId { get; set; }
 		public DateTime Time { get; set; } = DateTime.UtcNow;
 		public string UserId { get; set; } = default!;
 		public bool IsReported { get; set; }
 		public ApplicationUser User { get; set; } = default!;
+		public Comment? ParentComment { get; set; }
+		public ICollection<Comment> Replies { get; set; } = new List<Comment>();
 		public ICollection<Reaction> Reactions = new List<Reaction>();
 	}
 }
---
+++ InstagramProject.Repository/Migrations/20250707220130_AddCommentReplies.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstagramProject.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCommentReplies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentCommentId",
+                table: "comments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_ParentCommentId",
+                table: "comments",
+                column: "ParentCommentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_comments_comments_ParentCommentId",
+                table: "comments",
+                column: "ParentCommentId",
+                principalTable: "comments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_comments_comments_ParentCommentId",
+                table: "comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_comments_ParentCommentId",
+                table: "comments");
+
+            migrationBuilder.DropColumn(
+                name: "ParentCommentId",
+                table: "comments");
+        }
+    }
+}
