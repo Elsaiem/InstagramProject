95b04c402b6bcb1e594d0e743724d906c1859f42
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-07-25 02:05:08 +0300

    add react endpoint to add, remove react for post or comment

---
+++ InstagramProject.Core/Contracts/Reaction/CreateReactionRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramProject.Core.Contracts.Reaction
+{
+	public record CreateReactionRequest
+	(
+		int PostId,
+		int? CommentId
+	);
+}
---
+++ InstagramProject.Core/Errors/Reaction/ReactionErrors.cs
@@ -0,0 +1,13 @@
+﻿using InstagramProject.Core.Abstractions;
+using Microsoft.AspNetCore.Http;
+
+namespace InstagramProject.Core.Errors.Reaction
+{
+	public static class ReactionErrors
+	{
+		public static readonly Error UserNotFound = new("Reaction.UserNotFound", "User not found.", StatusCodes.Status404NotFound);
+		public static readonly Error PostNotFound = new("Reaction.PostNotFound", "Post not found.", StatusCodes.Status404NotFound);
+		public static readonly Error CommentNotFound = new("Reaction.CommentNotFound", "Comment not found.", StatusCodes.Status404NotFound);
+		public static readonly Error CommentNotInPost = new("Reaction.CommentNotInPost", "The comment does not belong to the specified post.", StatusCodes.Status400BadRequest);
+	}
+}
